b1bba6ff374f4689126b544d9b7458e09d2b087d
Umaraliyev0417 <umaraliyev0417@gmail.com>
2024-01-26 20:32:09 +0500

    Masalalar

---
+++ main.py
@@ -0,0 +1,129 @@
+# 1-masala <<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# a = 1
+# b = -12
+# c = -28
+# def quadratic_equation(a, b, c):
+#     d = ((b * b - 4 * a * c)) ** 0.5
+#     r = ((-b) + d) // (2 * a)
+#     return r
+#
+#
+# print(quadratic_equation(a, b, c))
+
+# 2-masala <<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# a = 1
+# b = 0
+# c = -1
+# def solutions(a, b, c):
+#     if (b * b - 4 * a * c) > 0:
+#         return 2
+#     elif (b * b - 4 * a * c) == 0:
+#         return 1
+#     else:
+#         return 0
+
+
+# print(solutions(a,b,c))
+
+
+# 3-masala <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<,
+
+# l1 = 2, 4, 1
+# l2 = 4, 2, 1
+# def lines_are_parallel(l1, l2):
+#     if l1[0] / l1[1] == l2[0] / l2[1]:
+#         return True
+#     else:
+#         return False
+#
+#
+# print(lines_are_parallel(l1, l2))
+
+
+# 4-masala <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# 5-masala <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# a = 25
+# b = 21
+# c = 125
+# def last_dig(a, b, c):
+#     a1 = a % 10
+#     b1 = b % 10
+#     c1 = c % 10
+#     c2 = a1 * b1
+#     c3 = c2 % 10
+#     if c3 == c1:
+#         return True
+#     else:
+#         return False
+#
+#
+# print(last_dig(a, b, c))
+
+
+# 6-masala <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# a = 1
+# b = 10
+# c = 4
+# def find_vertex(a, b, c):
+#     x = (-b) / (2 * a)
+#     y = a * x ** 2 + b * x + c
+#     return [x, y]
+#
+#
+# print(find_vertex(a, b, c))
+
+
+# 7-masala <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# 8-masala <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# num = 33
+# def is_repdigit(num):
+#     result = False
+#     num = str(num)
+#     for ch in num:
+#         if ch == num[0]:
+#             result = True
+#         else:
+#             result = False
+#     return result
+#
+#
+# print(is_repdigit(num))
+
+
+# 9-masala <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# base = 2
+# num = 64
+# def logarithm(base, num):
+#     for i in range(num):
+#         if base ** i == num:
+#             return i
+#     else:
+#         return "Invalid"
+#
+#
+# print(logarithm(base, num))
+
+
+# 10-masala  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# x = 3
+# y = 4
+# z = 5
+# def right_triangle(x, y, z):
+#     a, b, c = sorted([x, y, z])
+#     if a <= 0:
+#         return False
+#     return a ** 2 + b ** 2 == c ** 2
+#
+#
+# print(right_triangle(x, y, z))
